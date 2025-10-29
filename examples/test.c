69a948fbd6db9c39cc5a8f94496dd0ac05954efb
Hedy88 <diab.hedy@gmail.com>
2025-10-29 13:57:05 +0000

    CMake + other stuff

--- examples/test.c
+++ examples/test.c
@@ -4,7 +4,7 @@
 
 int main(){
 	mixer_t* mixer = mixer_open_device();
-	mixer_audio_t* audio = mixer_open_file(mixer, "aryx.s3m");
+	mixer_audio_t* audio = mixer_open_file(mixer, "./aryx.s3m");
 
 	mixer_audio_set_loop(mixer, audio, 1);
 	mixer_audio_start(mixer, audio);
